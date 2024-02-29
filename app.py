c768313daf959d6b674b683e2e90ae713784e52f
Jehwan Kim <jehwan.kim@kreimben.com>
2024-02-29 13:04:26 +0900

    Add logging and enhance model handling in app.py
    This update introduces better logging through Python's built-in logging module into app.py. The model handling efficiency has also been improved by moving the model loading and evaluation inside the lambda_handler function. This helps to avoid loading the model on every function invocation. Extra functionalities like custom image transformation and standard HTTP response handling are also added. The changes also streamline the error handling process, making it more robust and accurate.

--- app.py
+++ app.py
@@ -1,41 +1,77 @@
 import base64
 import json
+import logging
 from io import BytesIO
 
 import torch
 from PIL import Image
 from torchvision import transforms
 
-from src.module import ResNet, ResidualBlock
+from src.module import ResNet
 
-model = ResNet.load_from_checkpoint('mnist.ckpt',
-                                    block=ResidualBlock, layers=[2, 2, 2, 2], grayscale=True, batch_size=128)
-model.eval()
+# Configure logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)  # Set to DEBUG, INFO, WARNING, ERROR as needed
 
-# Define your transform pipeline
-transform = transforms.Compose([
-    transforms.Resize(28),
-    transforms.ToTensor()
-])
+
+# Custom transform for inverting image colors
+class InvertColours(object):
+    def __call__(self, img):
+        return Image.eval(img, lambda x: 255 - x)
+
+
+def response(code, message):
+    if code == 200:
+        key = "prediction"
+    else:
+        key = "error_message"
+
+    return {
+        "statusCode": code,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "OPTIONS,POST"
+        },
+        "body": json.dumps({key: message})
+    }
 
 
 def lambda_handler(event, context):
-    print(f'Received event: {json.dumps(event)}')
-    global model
-    print(f'after loading model: {json.dumps(model)}')
+    logger.info(f'Received event: {json.dumps(event)}')
+
+    model = ResNet.load_from_checkpoint('mnist.ckpt')  # , block=ResidualBlock, layers=[3, 4, 6, 3], grayscale=True)
+    model.eval()
+    logger.info('after loading model.')
+
+    # Define your transform pipeline
+    transform = transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),  # Convert image to grayscale
+        InvertColours(),  # change white to black, black to white.
+        transforms.Resize((28, 28)),
+        transforms.ToTensor(),
+    ])
 
     try:
         # Decode the image
-        body = json.loads(event['body'])
+        body = event['body']
+
         if body is None:
-            return json.dumps({'error': 'There are no body. please input a valid image throgh your request.'})
+            logger.error(f'No body received: {body=}')
+            return response(400, 'There are no body. please input a valid image throgh your request.')
+
+        # logger.info(f'{type(body)=}')
+        if type(body) == str: body = json.loads(body)
 
         if body['image'] is None:
-            return json.dumps({'error': 'There are no image. please input a valid image throgh your request.'})
+            logger.error(f'No image: {body=}')
+            return response(400, 'There are no image. please input a valid image throgh your request.')
 
         image_data = body['image']
         image_decoded = base64.b64decode(image_data)
         image = Image.open(BytesIO(image_decoded))
+        # logger.info(f'Received image: {image}')
 
         # Preprocess the image
         image_tensor = transform(image).unsqueeze(0)  # Add batch dimension
@@ -47,13 +83,8 @@ def lambda_handler(event, context):
         # Process the prediction as needed
         # For example, convert tensor to JSON serializable output
         prediction_result = prediction.argmax().item()
+        logger.info(f'prediction result: {prediction_result}')
 
-        return json.dumps({
-            'statusCode': 200,
-            'body': {'prediction': prediction_result}
-        })
+        return response(200, prediction_result)
     except Exception as e:
-        return json.dumps({
-            'statusCode': 400,
-            'body': f'{e}'
-        })
+        return response(500, f'{e}, {event}, {context}')
