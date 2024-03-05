fb2794a0002abfc68c9699e517365dddd227898d
Jehwan Kim <jehwan.kim@kreimben.com>
2024-03-05 11:33:45 +0900

    Optimize DataLoader usage in ResNet class
    The previous implementation of DataLoader in train, test, and validation data loader methods had repetitive code. This refactoring utilizes a dictionary (self.dataset_hp) containing DataLoader parameters to streamline the DataLoader calls. It also inserts device preference option in the ResNet class initializer. The compelling advantage of this approach is its ability to maintain clean and DRY (Don't Repeat Yourself) code.

--- src/module.py
+++ src/module.py
@@ -43,7 +43,7 @@ class ResidualBlock(nn.Module):
 
 class ResNet(L.LightningModule):
     def __init__(self, block, layers, num_classes=10, grayscale=False, learning_rate: float = 1e-5,
-                 batch_size: int = 128):
+                 batch_size: int = 128, device='cpu'):
         super(ResNet, self).__init__()
         self.save_hyperparameters()
 
@@ -55,6 +55,13 @@ class ResNet(L.LightningModule):
         self.learning_rate = learning_rate
         self.batch_size = batch_size
         self.accuracy = Accuracy('multiclass', num_classes=num_classes)
+        self.dataset_hp = {
+            'pin_memory': True,
+            'batch_size': self.hparams.batch_size,
+            'num_workers': os.cpu_count() - 1,
+            'generator': torch.Generator(device=device),
+            'persistent_workers': True,
+        }
 
         if grayscale:
             in_dim = 1
@@ -118,17 +125,17 @@ class ResNet(L.LightningModule):
 
     def train_dataloader(self):
         dataset = datasets.MNIST(root='./data', train=True, download=True, transform=self.transform)
-        loader = DataLoader(dataset, batch_size=self.hparams.batch_size, shuffle=True, num_workers=os.cpu_count() - 1)
+        loader = DataLoader(dataset, shuffle=True, **self.dataset_hp)
         return loader
 
     def val_dataloader(self):
         dataset = datasets.MNIST(root='./data', train=False, download=True, transform=self.transform)
-        loader = DataLoader(dataset, batch_size=self.hparams.batch_size, shuffle=False, num_workers=os.cpu_count() - 1)
+        loader = DataLoader(dataset, **self.dataset_hp)
         return loader
 
     def test_dataloader(self):
         dataset = datasets.MNIST(root='./data', train=False, download=True, transform=self.transform)
-        loader = DataLoader(dataset, batch_size=self.hparams.batch_size, shuffle=False, num_workers=os.cpu_count() - 1)
+        loader = DataLoader(dataset, **self.dataset_hp)
         return loader
 
     def training_step(self, batch, batch_idx):
@@ -155,116 +162,3 @@ class ResNet(L.LightningModule):
         acc = self.accuracy(outputs, labels)
         self.log('test_loss', loss, prog_bar=True)
         self.log('test_acc', acc, prog_bar=True)
-
-# class ResNet(L.LightningModule):
-#     def __init__(self, block, layers, grayscale, learning_rate: float = 1e-5, batch_size: int = 64):
-#         super().__init__()
-#         self.save_hyperparameters()
-#
-#         self.learning_rate = learning_rate
-#         self.batch_size = batch_size
-#         self.inplanes = 64
-#         self.num_worker = os.cpu_count() - 1
-#         self.loss = nn.CrossEntropyLoss()
-#         self.accuracy = Accuracy()
-#
-#         if grayscale:
-#             in_dim = 1
-#         else:
-#             in_dim = 3
-#         self.conv1 = nn.Conv2d(in_dim, 64, kernel_size=7, stride=2, padding=3,
-#                                bias=False)
-#         self.bn1 = nn.BatchNorm2d(64)
-#         self.relu = nn.ReLU(inplace=True)
-#         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-#         self.layer1 = self._make_layer(block, 64, layers[0])
-#         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-#         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-#         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-#         self.avgpool = nn.AvgPool2d(7, stride=1)
-#         self.fc = nn.Linear(512 * block.expansion, 10)
-#
-#         for m in self.modules():
-#             if isinstance(m, nn.Conv2d):
-#                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-#                 m.weight.data.normal_(0, (2. / n) ** .5)
-#             elif isinstance(m, nn.BatchNorm2d):
-#                 m.weight.data.fill_(1)
-#                 m.bias.data.zero_()
-#
-#     def _make_layer(self, block, planes, blocks, stride=1):
-#         downsample = None
-#         if stride != 1 or self.inplanes != planes * block.expansion:
-#             downsample = nn.Sequential(
-#                 nn.Conv2d(self.inplanes, planes * block.expansion,
-#                           kernel_size=1, stride=stride, bias=False),
-#                 nn.BatchNorm2d(planes * block.expansion),
-#             )
-#
-#         layers = []
-#         layers.append(block(self.inplanes, planes, stride, downsample))
-#         self.inplanes = planes * block.expansion
-#         for i in range(1, blocks):
-#             layers.append(block(self.inplanes, planes))
-#
-#         return nn.Sequential(*layers)
-#
-#     def forward(self, x):
-#         x = self.conv1(x)
-#         x = self.bn1(x)
-#         x = self.relu(x)
-#         x = self.maxpool(x)
-#
-#         x = self.layer1(x)
-#         x = self.layer2(x)
-#         x = self.layer3(x)
-#         x = self.layer4(x)
-#         # because MNIST is already 1x1 here:
-#         # disable avg pooling
-#         # x = self.avgpool(x)
-#
-#         x = x.view(x.size(0), -1)
-#         logits = self.fc(x)
-#         # probas = F.softmax(logits, dim=1)
-#         return logits  # , probas
-#
-#     def training_step(self, batch, batch_idx):
-#         inputs, labels = batch
-#         outputs = self(inputs)
-#         loss = self.loss(outputs, labels)
-#         self.log('train_loss', loss, prog_bar=True)
-#         return loss
-#
-#     def validation_step(self, batch, batch_idx):
-#         inputs, labels = batch
-#         outputs = self(inputs)
-#         loss = self.loss(outputs, labels)
-#         acc = self.accuracy(outputs, labels)
-#         self.log('val_loss', loss, prog_bar=True)
-#         self.log('val_acc', acc, prog_bar=True)
-#         return loss
-#
-#     def test_step(self, batch, batch_idx):
-#         inputs, labels = batch
-#         outputs = self(inputs)
-#         loss = self.loss(outputs, labels)
-#         acc = self.accuracy(outputs, labels)
-#         self.log('test_loss', loss, prog_bar=True)
-#         self.log('test_acc', acc, prog_bar=True)
-#         return loss
-#
-#     def configure_optimizers(self):
-#         optimizer = torch.optim.Adam(self.parameters(), self.learning_rate)
-#         return optimizer
-#
-#     def train_dataloader(self):
-#         dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
-#         return DataLoader(dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_worker)
-#
-#     def val_dataloader(self):
-#         dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
-#         return DataLoader(dataset, batch_size=self.batch_size, num_workers=self.num_worker)
-#
-#     def test_dataloader(self):
-#         dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
-#         return DataLoader(dataset, batch_size=self.batch_size, num_workers=self.num_worker)
